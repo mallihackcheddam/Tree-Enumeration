7eae92639e3cc66cab046d6c33b7a8f827834b64
Nikhil1357 <lakshmim1357@gmail.com>
2023-10-05 19:11:22 +0530

    Minor UI changes ~ Abhiram

--- frontend/src/components/UserAgency/Upload.js
+++ frontend/src/components/UserAgency/Upload.js
@@ -10,75 +10,74 @@ import axios from 'axios';
 
 function Upload() {
 
-    const [file, setfile] = useState(null);
-    const [location, setlocation] = useState("");
-    const [img, setImg] = useState({ upload });
-    const [count,setCount]= useState(0);
-    const {state}  = useLocation();
-    const [response, setResponse] = useState(null);
+  const [file, setfile] = useState(null);
+  const [location, setlocation] = useState("");
+  const [img, setImg] = useState({ upload });
+  const [count, setCount] = useState(0);
+  const { state } = useLocation();
+  const [response, setResponse] = useState(null);
 
-  const {email} = state;
+  const { email } = state;
   const handleSubmit = () => {
     const formData = new FormData();
     //user email is in loc.email
-  
-    if(location==="")alert("Please choose a location");
+
+    if (location === "") alert("Please choose a location");
     formData.append("file", file, file.name);
     formData.append("location", location);
     formData.append("email", email);
 
     axios.post("/upload", formData).then((data) => {
-        setResponse(data.data)
+      setResponse(data.data)
     });
   };
 
-    return (
-        <div className="govtupload">
-            <UserNavbar />
-            <div className="totall">
-                <div className="mb-3 totalmap">
-                    <div className="leftmap">
-                        <input
-                            onChange={(e) => {
-                                setfile(e.target.files[0]);
-                                setImg(e.target.files[0]);
-                            }}
-                            className="" type="file" id="formFile" accept='.jpeg, .png, .jpg' />
-                    </div>
+  return (
+    <div className="govtupload">
+      <UserNavbar />
+      <div className="totall">
+        <div className="mb-3 totalmap">
+          <div className="leftmap">
+            <input
+              onChange={(e) => {
+                setfile(e.target.files[0]);
+                setImg(e.target.files[0]);
+              }}
+              className="" type="file" id="formFile" accept='.jpeg, .png, .jpg' />
+          </div>
 
-                    <div style={{
-                      width:'30vw',
-                      height:'60vh',
-                    }}>
-                        <div
-                        style={{
+          <div style={{
+            width: '30vw',
+            height: '60vh',
+          }}>
+            <div
+              style={{
 
-                        height:'20%',
-                      overflow:'hidden',
-                      textOveflow:'ellipsis', 
-                      width:'inherit',
-                        }}  
-                        >Select Location : {location}</div>
-                        <MapLocate setlocation={setlocation} />
+                height: '20%',
+                overflow: 'hidden',
+                textOveflow: 'ellipsis',
+                width: 'inherit',
+              }}
+            >Select Location : {location}</div>
+            <MapLocate setlocation={setlocation} />
 
-                    </div>
+          </div>
 
-                </div>
-                <button
-                    onClick={() => { handleSubmit() }}
-                    className='btn btn-primary'>
-                    Submit
-                </button>
-                <div className="results">
-                    <h1>Results</h1>
-                    <p>Total count : {response?response.total_count:0}</p>
-                </div>
-                <div
-                style={{
-                  width:'15%'
-                }}
-                >
-                {/* <div
+        </div>
+        <button
+          onClick={() => { handleSubmit() }}
+          className='btn btn-primary'>
+          Submit
+        </button>
+        <div className="results">
+          <h1>{response?"Results":""}</h1>
+        </div>
+        <div
+          style={{
+            width: '15%'
+          }}
+        >
+          {/* <div
                 style={{
                   marginBottom:4,
                   width:"200px",
@@ -86,28 +85,23 @@ function Upload() {
                   backgroundColor:"black",
                 }}
                 ></div> */}
-                  {response?
-                  response.species.map((val)=>{
-                    return <>
-                      <div 
-                      style={{
-                        display:'flex',
-                        justifyContent:"space-around",
-                      }}
-                      >
-
-
-                      <p>{val.name} : </p>
-                      <p>{val.count}</p>
-
-                      </div>
-                    </>
-                  })
-                  :<></>}
+          <div>{response?<div className='tablebro'><div>Name</div><div>Count</div></div>:<div></div>}</div>
+          {response ?
+            response.species.map((val) => {
+              return <>
+                <div className='tablebro'>
+                  <div>{val.name}</div>
+                  <div>{val.count}</div>
                 </div>
-            </div>
+              </>
+            })
+            : <></>}
+            {response?<p>Total count : {response ? response.total_count : 0}</p>:<></>}
         </div>
-    )
-  }
-  export default Upload;
-  
+        
+      </div>
+    </div>
+  )
+}
+export default Upload;
+
